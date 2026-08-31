73774b11fddda684a10f8e51809a6dbd866d25c6
Sibsankar335 <dsibsankar088@gmail.com>
2026-08-31 18:53:07 +0530

    Refactor database connection in config.php
    Updated database connection settings to use SSL and new credentials.

--- config.php
+++ config.php
@@ -1,6 +1,31 @@
 <?php
- $con=mysqli_connect("localhost","root",'','login');
- if(!$con){
-    echo "something went wrong";
- }
- ?>
+
+$host = "mysql-dfa08d4-dsibsankar088-e1d8.i.aivencloud.com";
+$user = "avnadmin";
+$password = "AVNS_VoXXKXueCeEkLhQzeXF";
+$database = "defaultdb";
+$port =23204;
+
+$con = mysqli_init();
+
+mysqli_ssl_set(
+    $con,
+    null,
+    null,
+    null,
+    null,
+    null
+);
+
+if (!mysqli_real_connect(
+    $con,
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+)) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+?>
